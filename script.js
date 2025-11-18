e1d74bf6534099075c8b4ebf39c5b2aef9eddd8b
HIBA53757 <hibatallahsadiki20@gmail.com>
2025-11-18 22:13:04 +0100

    add modal html et css + respensive + open modal js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const addWorkerBtn = document.getElementById('addWorker')
+const Modal = document.getElementById('Modal');
+const cancelBtn =document.getElementById('cancelBtn')
+
+addWorkerBtn.addEventListener('click',()=>{
+    Modal.style.display ='flex'
+});
+cancelBtn.addEventListener('click',()=>{
+ Modal.style.display ="none"
+});
+
+const addExperienceBtn = document.getElementById('addExperience')
+
+addExperienceBtn.addEventListener('click',()=>{
+    
+})
